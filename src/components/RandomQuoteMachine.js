3780a5292895d867a26b9d3f580e410ee0c9446d
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-08-13 00:49:30 +0700

    Optimize

--- src/components/RandomQuoteMachine.js
+++ src/components/RandomQuoteMachine.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from "react"
 import RandomQuoteMachineComponent from "./RandomQuoteMachineComponents";
+import QuoteDB_En from "./RandomQuoteMachineData_En"
+import QuoteDB_Vi from "./RandomQuoteMachineData_Vi"
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 import random from "../helpers/random";
 import randomColor from "../helpers/randomColor";
-import QuoteDB_En from "./RandomQuoteMachineData_En"
-import QuoteDB_Vi from "./RandomQuoteMachineData_Vi"
 
 function RandomQuoteMachine() {
 
@@ -24,39 +24,17 @@ function RandomQuoteMachine() {
         />
     )
 
-    const handleLang = () => {
-        if(lang === "En") {
-            setLang(lang => "Vi")
-        } else if(lang === "Vi") {
-            setLang(lang => "En")
-        }
-    }
+    const handleLang = () => lang === "En" ? setLang(lang => "Vi") : setLang(lang => "En")
 
     const handleQuotes = () => {
-
-        let quoteData = lang === "En" 
-        ? QuoteDB_En
-        : QuoteDB_Vi
-
+        const quoteData = ( lang === "En" ? QuoteDB_En : QuoteDB_Vi )
         const index = random(0, quoteData.length - 1)
         setText(text => quoteData[index].text)
-        setAuthor(author => quoteData[index].author == null ? (lang == "En" ? "Unknown" : "Sưu tầm") : quoteData[index].author)
+        setAuthor(author => quoteData[index].author === null ? (lang === "En" ? "Unknown" : "Sưu tầm") : quoteData[index].author)
         setBackground(background => randomColor())
     }
 
-    // const handleQuotes = () => {
-    //     fetch("https://type.fit/api/quotes")
-    //         .then(response => response.json())
-    //         .then(QuoteDB_En => {
-    //             const index = random(0, QuoteDB_En.length - 1)
-    //             setText(text => QuoteDB_En[index].text)
-    //             setAuthor(author => QuoteDB_En[index].author == null ? "Unknown" : QuoteDB_En[index].author)
-    //             setBackground(background => randomColor())
-    //         }) 
-    // }
-
     return (
-
         <RandomQuoteMachineComponent 
             text = {text}
             author = {author}
@@ -66,7 +44,6 @@ function RandomQuoteMachine() {
             handleLang = {() => handleLang()}
             lang = {lang}
         />
-
     )
 }
 
